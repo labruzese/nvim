9258f02255904d4ae60391e3803b3d665cadfb6c
Skylar Abruzese <skylar@abruzese.com>
2026-04-11 00:03:37 -0400

    add telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,18 +10,21 @@ vim.pack.add({
 	gh('kdheepak/lazygit.nvim'),
 	gh('folke/which-key.nvim'),
 	gh('nvim-lua/plenary.nvim'),
-	{ src = gh('ThePrimeagen/harpoon'), version = 'harpoon2' }
+	{ src = gh('ThePrimeagen/harpoon'), version = 'harpoon2' },
+	gh('nvim-telescope/telescope.nvim'),
+	gh('nvim-telescope/telescope-fzf-native.nvim'),
 })
 
 vim.cmd('colorscheme gruvbox')
 
 require('nvim-autopairs').setup()
 require('oil').setup()
-local hpn = require('harpoon').setup()
 
+-- harpoon
+local hpn = require('harpoon')
+hpn.setup();
 vim.keymap.set('n', '<leader>a', function() hpn:list():add() end)
 vim.keymap.set('n', '<C-e>', function() hpn.ui:toggle_quick_menu(hpn:list()) end)
-
 vim.keymap.set('n', '<A-q>', function() hpn:list():select(1) end)
 vim.keymap.set('n', '<A-w>', function() hpn:list():select(2) end)
 vim.keymap.set('n', '<A-e>', function() hpn:list():select(3) end)
@@ -29,8 +32,39 @@ vim.keymap.set('n', '<A-r>', function() hpn:list():select(4) end)
 vim.keymap.set('n', '<C-S-P>', function() hpn:list():prev() end)
 vim.keymap.set('n', '<C-S-N>', function() hpn:list():next() end)
 
+-- telescope fzf compile if not present
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == 'telescope-fzf-native.nvim' and (kind == 'install' or kind == 'update') then
+      vim.system({ 'make' }, { cwd = ev.data.path }):wait()
+    end
+  end,
+})
+-- telescope
+require('telescope').setup({
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = 'smart_case',
+    },
+  },
+})
+require('telescope').load_extension('fzf')
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+vim.keymap.set('n', '<leader>fs', builtin.git_status)
+
+-- lazygit
 vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>')
+
+-- oil
 vim.keymap.set('n', '-', '<cmd>Oil<CR>')
 
--- relevant external config
+-- treesitter
 require('autocmds.autosyntax')
