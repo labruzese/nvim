fbd94a09706976d78ef31afe392ebbe58b6d4976
Skylar Abruzese <skylar@abruzese.com>
2026-05-06 11:54:12 -0400

    osc 52 support

--- init.lua
+++ init.lua
@@ -12,6 +12,20 @@ vim.o.winborder = 'rounded'
 vim.g.rustfmt_emit_files = 1
 vim.g.rustfmt_command = "rustup run nightly rustfmt"
 
+if os.getenv("SSH_TTY") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
+
 require('lsp')
 require('plugins')
 require('statusline')
